07fa30c6cd03076b5d1ac4eca2cb0e324ca2b132
Nitesh Patel <singhpatelsjd@gmail.com>
2026-07-14 13:12:56 +0530

    updated mail utils

--- src/utils/mailUtil.js
+++ src/utils/mailUtil.js
@@ -1,10 +1,25 @@
 const nodemailer = require("nodemailer");
 
+const mailPort =
+  Number(process.env.MAIL_PORT) || 587;
+
+const mailSecure =
+  process.env.MAIL_SECURE
+    ? process.env.MAIL_SECURE === "true"
+    : mailPort === 465;
+
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  family: 4,
+  host:
+    process.env.MAIL_HOST ||
+    "smtp.gmail.com",
+  port: mailPort,
+  secure: mailSecure,
+  family:
+    Number(process.env.MAIL_FAMILY) ||
+    4,
+  connectionTimeout: 15000,
+  greetingTimeout: 15000,
+  socketTimeout: 30000,
   auth: {
     user: process.env.MAIL_USERNAME,
     pass: process.env.MAIL_PASSWORD,
